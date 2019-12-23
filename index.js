7ceb845fb9df369806781943ab9c82015c6cc107
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-23 16:02:38 -0300

    added support to more music extensions

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const path = require('path')
 fs.readdir(__dirname, (err, files) => {
     files.forEach(async file => {
         const newFile = file.replace('y2mate.com - ', '').split('_')
+        const extension = path.extname(file)
 
-        file.includes('.mp3') && newFile.length > 1 && await fs.rename(
-            path.join(__dirname, file),
-            path.join(__dirname, `${newFile.slice(0, newFile.length - 1).map(text => text.length > 2 ? `${text[0].toUpperCase()}${text.substring(1, text.length)}` : text).join(' ')}.mp3`),
-            () => { }
-        )
+        if ((extension === '.mp3' || extension === '.mp4' || extension === '.3gp' || extension === '.m4a' || extension === '.webm') && newFile.length > 1) {
+            await fs.rename(
+                path.join(__dirname, file),
+                path.join(__dirname, `${newFile.slice(0, newFile.length - 1).map(text => text.length > 2 ? `${text[0].toUpperCase()}${text.substring(1, text.length)}` : text).join(' ')}${extension}`),
+                () => { }
+            )
+        }
     })
 })
