30a3f4124473ae79b346a3f21563cc218e2046d9
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-24 17:24:31 -0300

    filter and now it moves the renamed file
    the renamed file is now being moved into the output folder

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const fs = require('fs')
 const path = require('path')
 
 fs.readdir(__dirname, (err, files) => {
-    files.forEach(async file => {
-        const newFile = file.replace('y2mate.com - ', '').split('_')
-        const extension = path.extname(file)
+    if (err) throw new Error(err)
 
-        if ((extension === '.mp3' || extension === '.mp4' || extension === '.3gp' || extension === '.m4a' || extension === '.webm') && newFile.length > 1) {
-            await fs.rename(
-                path.join(__dirname, file),
-                path.join(__dirname, `${newFile.slice(0, newFile.length - 1).map(text => text.length > 2 ? `${text[0].toUpperCase()}${text.substring(1, text.length)}` : text).join(' ')}${extension}`),
-                () => { }
-            )
-        }
-    })
+    if (!fs.existsSync(path.join(__dirname, 'output'))) fs.mkdirSync('output')
+
+    files
+        .filter(file => ['.mp3', '.mp4', '3gp', '.m4a', '.webm'].indexOf(path.extname(file)) !== -1)
+        .forEach(async file => {
+            const newFile = file.replace('y2mate.com - ', '').split('_')
+            const extension = path.extname(file)
+
+            if (newFile.length > 1) {
+                const newName = newFile.slice(0, newFile.length - 1).map(text => text.length > 2 ? `${text[0].toUpperCase()}${text.substring(1, text.length)}` : text).join(' ')
+
+                await fs.copyFile(
+                    path.join(__dirname, file),
+                    path.join(__dirname, `./output/${newName}${extension}`),
+                    () => console.log(`\x1b[31m${file}\x1b[0m moved to output folder as \x1b[32m${newName}${extension}\x1b[0m`)
+                )
+            }
+        })
 })
