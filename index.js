4b7c8fed66c9e055adec9abe0385091af9702b51
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-25 00:37:27 -0300

    removed unnecessary async await

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ fs.readdir(__dirname, (err, files) => {
 
     files
         .filter(file => ['.mp3', '.mp4', '3gp', '.m4a', '.webm'].indexOf(path.extname(file)) !== -1)
-        .forEach(async file => {
+        .forEach(file => {
             const newFile = file.replace('y2mate.com - ', '').split('_')
             const extension = path.extname(file)
 
             if (newFile.length > 1) {
                 const newName = newFile.slice(0, newFile.length - 1).map(text => text.length > 2 ? `${text[0].toUpperCase()}${text.substring(1, text.length)}` : text).join(' ')
 
-                await fs.copyFile(
+                fs.copyFile(
                     path.join(__dirname, file),
                     path.join(__dirname, `./output/${newName}${extension}`),
                     () => console.log(`\x1b[31m${file}\x1b[0m \x1b[37mmoved to output folder as\x1b[0m \x1b[32m${newName}${extension}\x1b[0m`)
